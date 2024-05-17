db04c90347ba6553661fa19fa232b43a7fe04c5e
Joao <joao.schier@coreflux.org>
2024-05-17 16:43:22 +0100

    Fixing Architecture

---
+++ ProjectShelly/Classes/Company.cs
@@ -0,0 +1,13 @@
+﻿using App.WorkerService;
+using Coreflux.API.Networking.MQTT;
+using Newtonsoft.Json;
+namespace Classes;
+
+public class Company
+{
+    public string Name { get; set; } = "";
+
+	public string Topic{get; set;} = "";
+	public double TotalEnergy;
+    public List<Office> Offices { get; set; } = new List<Office>();
+}
