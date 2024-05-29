9d157ebea3cd64564fae9e301b8fd4c46527fd59
João Schier <joao.schier@coreflux.org>
2024-05-29 11:45:29 +0100

    Test RunService

--- Rita/Build.cs
+++ Rita/Build.cs
@@ -39,7 +39,7 @@ using static Nuke.Common.IO.PathConstruction;
 )]
 class Build : NukeBuild
 {
-    public static int Main() => Execute<Build>(x => x.Init, x => x.CreateService);
+    public static int Main() => Execute<Build>(x => x.Init, x => x.RunService);
 
     [Solution]
     readonly Solution Solution;
